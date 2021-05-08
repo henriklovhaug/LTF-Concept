648ec990a3e9d67dd498375ec9258546f1751ac3
Henrik <henriktl@stud.ntnu.no>
2021-05-08 12:56:54 +0200

    CMAKE made

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
-#include <iostream>
+#include<iostream>
+#include"raylib.h"
+int main(void)
+{
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "The_big_hen");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Ababot", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        
+    }
+
+    CloseWindow(); 
+
+    return 0;
 }
