95c6c26d7e19952a0100b65f7f6e3a006bf33d9a
Henrik <henriktl@stud.ntnu.no>
2021-05-10 08:37:26 +0200

    testing

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
 #include "raylib.h"
 #include "player.h"
-#include <iostream>
+#include<iostream>
+#include<string>
+#include <sstream>
 
 #define MAX_COLUMNS 20
+
 
 Player player(4.0f, 2.0f, 4.0f);
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800 * 3;
-    const int screenHeight = 450 * 3;
+    const int screenWidth = 800;
+    const int screenHeight = 450 ;
 
     InitWindow(screenWidth, screenHeight, "Henrik & Stefan");
 
     // Define the camera to look into our 3d world (position, target, up vector)
     Camera camera = {0};
-    camera.position = {player.getX(), player.getZ(), player.getY()};
-    camera.target = {0.0f, 1.8f, 0.0f};
+    camera.position = {player.getX(), player.getY(), player.getZ()};
     camera.up = {0.0f, 1.0f, 0.0f};
     camera.fovy = 60.0f;
 
@@ -43,7 +45,8 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-         if (IsKeyDown('W'))
+        //keyboard inputs
+        if (IsKeyDown('W'))
         {
             player.moveForward();
         }
@@ -59,7 +62,7 @@ int main(void)
         {
             player.moveRight();
         }
-        camera.position = {player.getX(), player.getZ(), player.getY()};
+        camera.position = {player.getX(), player.getY(), player.getZ()};
         UpdateCamera(&camera); // Update camera
         //----------------------------------------------------------------------------------
 
@@ -70,7 +73,7 @@ int main(void)
         ClearBackground(RAYWHITE);
 
         BeginMode3D(camera);
-
+        
         DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f}, LIGHTGRAY); // Draw ground
         DrawCube({-16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, BLUE);  // Draw a blue wall
         DrawCube({16.0f, 2.5f, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);   // Draw a green wall
@@ -91,11 +94,18 @@ int main(void)
         DrawText("First person camera default controls:", 20, 20, 10, BLACK);
         DrawText("- Move with keys: W, A, S, D", 40, 40, 10, DARKGRAY);
         DrawText("- Mouse move to look around", 40, 60, 10, DARKGRAY);
-    std::cout << player.getX() << std::endl;
-*/
+        std::cout << player.getX() << std::endl;
+        */
+       std::string mystring = std::to_string(camera.target.x);
+        std::cout << "target x: " << mystring << std::endl;
+       std::string mystriny = std::to_string(camera.target.y);
+        std::cout << "target y: " << mystriny << std::endl;
+       std::string mystrinz = std::to_string(camera.target.z);
+        std::cout << "target z: " << mystrinz << std::endl;
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
+    
     // De-Initialization
     //--------------------------------------------------------------------------------------
     CloseWindow(); // Close window and OpenGL context
